2f9b09b306e39134af25a11b4cc71f269dd52a59
yokoyokohiyoko <63271567+yokoyokohiyoko@users.noreply.github.com>
2021-12-13 18:41:09 +0900

    henkou

--- src/index.js
+++ src/index.js
@@ -59,11 +59,16 @@
 // const sayHello = (name = "yoko") => console.log(`こんにちは!${name}さん!`)
 
 // sayHello("ひよこ")
-const arr1 = [1, 2]
-// console.log(arr1)
+// const arr1 = [1, 2]
+// // console.log(arr1)
 
-// console.log(...arr1)
+// // console.log(...arr1)
 
-const sumFunc = (num1, num2) => console.log(num1 + num2)
-sumFunc(arr1[0], arr1[1])
-sumFunc(...arr1)
+// const sumFunc = (num1, num2) => console.log(num1 + num2)
+// sumFunc(arr1[0], arr1[1])
+// sumFunc(...arr1)
+
+const arr2 = [1, 2, 3, 4, 5]
+const [num1, num2, ...arr3] = arr2
+console.log(num1)
+console.log(num2)
