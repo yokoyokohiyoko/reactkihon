1d62a071a549b67277c3e71d85c5a533c41cba17
yokoyokohiyoko <63271567+yokoyokohiyoko@users.noreply.github.com>
2021-12-13 18:57:32 +0900

    henkou

--- src/index.js
+++ src/index.js
@@ -68,7 +68,12 @@
 // sumFunc(arr1[0], arr1[1])
 // sumFunc(...arr1)
 
-const arr2 = [1, 2, 3, 4, 5]
-const [num1, num2, ...arr3] = arr2
-console.log(num1)
-console.log(num2)
+//
+const arr4 = [10, 20]
+const arr5 = [30, 40]
+
+const arr6 = [...arr4]
+console.log(arr6)
+
+const arr7 = [...arr4, ...arr5]
+console.log(arr7)
